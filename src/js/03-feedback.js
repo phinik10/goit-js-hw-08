d103c82c8469798fc6e2f15ba8f2270c66f84610
Anastasia <123020355+phinik10@users.noreply.github.com>
2023-09-01 18:38:31 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,10 +5,10 @@ const emailInput = form.querySelector('input[name="email"]');
 const messageTextarea = form.querySelector('textarea[name="message"]');
 const storageKey = 'feedback-form-state';
 
-// Зчитуємо дані з локального сховища
+
 const savedData = JSON.parse(localStorage.getItem(storageKey)) || {};
 
-// Заповнюємо поля форми зі зчитаними даними
+
 if (savedData.email) {
   emailInput.value = savedData.email;
 }
@@ -17,7 +17,7 @@ if (savedData.message) {
   messageTextarea.value = savedData.message;
 }
 
-// Функція, яка зберігає дані в локальному сховищі
+
 const saveDataToLocalStorage = () => {
   const dataToSave = {
     email: emailInput.value,
@@ -27,24 +27,24 @@ const saveDataToLocalStorage = () => {
   localStorage.setItem(storageKey, JSON.stringify(dataToSave));
 };
 
-// Встановлюємо обробники подій для полів форми
+
 emailInput.addEventListener('input', throttle(saveDataToLocalStorage, 500));
 messageTextarea.addEventListener('input', throttle(saveDataToLocalStorage, 500));
 
-// Обробник події сабміту форми
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  // Отримуємо дані з полів форми
+  
   const email = emailInput.value;
   const message = messageTextarea.value;
 
-  // Виводимо їх у консоль
+  
   console.log('Form Data:');
   console.log('Email:', email);
   console.log('Message:', message);
 
-  // Очищаємо локальне сховище та поля форми
+  
   localStorage.removeItem(storageKey);
   emailInput.value = '';
   messageTextarea.value = '';
